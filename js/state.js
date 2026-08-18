317208ace9b5ff4653a6bf2b96ba731a54e86366
Marc Segura <marcsegurallado@gmail.com>
2026-08-18 21:11:57 +0200

    Remove personal defaults from app state

--- js/state.js
+++ js/state.js
@@ -4,12 +4,12 @@ export const LEGACY_KEYS = ['healthCoachV2', 'healthCoachMvpV1'];
 const defaultState = {
   version: 3,
   settings: {
-    name: 'Marc',
-    age: 35,
-    height: 176.5,
-    sex: 'male',
-    goal: 85,
-    targetDate: '2026-11-30'
+    name: '',
+    age: null,
+    height: null,
+    sex: null,
+    goal: null,
+    targetDate: ''
   },
   profile: null,
   days: [],
